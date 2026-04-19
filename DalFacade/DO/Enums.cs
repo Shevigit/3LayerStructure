079d6cc4f18e964502188390a9135d8f6c7c02df
BatShevagit <sh0548503408@gmail.com>
2026-04-19 20:09:02 +0300

    change the enum

--- DalFacade/DO/Enums.cs
+++ DalFacade/DO/Enums.cs
@@ -2,4 +2,4 @@
 
 namespace DO;
 
-public enum categoryProduct { משחקי_חשיבה, משחקי_הרכבה, משחקי_קופסא, משחקי_בובות, משחקי_קלפים }
+public enum categoryProduct { משחקי_הרכבה, משחקי_קופסא, משחקי_חשיבה, משחקי_קלפים, משחקי_בובות }
